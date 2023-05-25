a58f3ed3df0596fa61c5918972f4b191a057cfd1
The Ghost <tghostlycat@gmail.com>
2023-05-25 11:50:00 +0400

    chore: user settings page

---
+++ src/pages/user/settings/index.tsx
@@ -0,0 +1,278 @@
+import { useSession } from "next-auth/react";
+import { InputText } from "primereact/inputtext";
+import { Password } from "primereact/password";
+import { TabPanel, TabView } from "primereact/tabview";
+import { useEffect, useState } from "react";
+
+import CsButton from "@/components/CsButton";
+import AccountLayout from "@/layouts/AccountLayout";
+import { apiClient } from "@/utils/api";
+
+export default function SettingsPage() {
+  const [values, setValues] = useState({
+    email: "",
+    first_name: "",
+    last_name: "",
+    phone: "",
+    telegram: "",
+    whatsapp: "",
+    instagram: "",
+  });
+
+  const [securityValues, setSecurityValues] = useState({
+    password: "",
+    password_confirmation: "",
+  });
+
+  const { data: sessionData, status } = useSession();
+
+  interface SocialNetworks {
+    telegram: string;
+    whatsapp: string;
+    instagram: string;
+  }
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      const initialFetch = async () => {
+        console.log("initial fetch");
+        await fetchUserData();
+      };
+      void initialFetch();
+    }
+  }, []);
+
+  const fetchUserData = async () => {
+    console.log("Fetching started...");
+
+    const userData = await apiClient.users.me.query();
+    console.log("Fetching started...");
+    if (userData) {
+      const socialNetworks: SocialNetworks = userData.social_networks
+        ? (JSON.parse(userData.social_networks as string) as SocialNetworks)
+        : {
+            telegram: "",
+            whatsapp: "",
+            instagram: "",
+          };
+      setValues({
+        email: userData.email ?? "",
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        phone: userData.phone ?? "",
+        telegram: socialNetworks.telegram,
+        whatsapp: socialNetworks.whatsapp,
+        instagram: socialNetworks.instagram,
+      });
+    }
+  };
+
+  const handleChange = (
+    prop:
+      | "email"
+      | "first_name"
+      | "last_name"
+      | "phone"
+      | "telegram"
+      | "whatsapp"
+      | "instagram",
+    event: { target: { value: string } }
+  ) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleSecurityChange = (
+    prop: "password" | "password_confirmation",
+    event: { target: { value: string } }
+  ) => {
+    setSecurityValues({ ...securityValues, [prop]: event.target.value });
+  };
+
+  const updateUserData = async () => {
+    await apiClient.users.update.query(values);
+    await fetchUserData();
+  };
+
+  const updatePassword = async () => {
+    await apiClient.users.updatePassword.query(securityValues);
+    await fetchUserData();
+  };
+
+  return (
+    <AccountLayout role="leaseholder" selectedOption="settings">
+      <div className="section">
+        <TabView>
+          <TabPanel header="Основные данные">
+            <div className="max-w-xl">
+              <div className="mb-6 grid grid-cols-2 gap-4">
+                <div className="flex flex-col gap-3">
+                  <label
+                    className="text-lg font-medium text-white"
+                    htmlFor="first_name"
+                  >
+                    Имя
+                  </label>
+                  <InputText
+                    className="p-inputtext-sm"
+                    id="first_name"
+                    value={values.first_name}
+                    onChange={(e) => {
+                      handleChange("first_name", e);
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <label
+                    className="text-lg font-medium text-white"
+                    htmlFor="last_name"
+                  >
+                    Фамилия
+                  </label>
+                  <InputText
+                    className="p-inputtext-sm"
+                    id="last_name"
+                    value={values.last_name}
+                    onChange={(e) => {
+                      handleChange("last_name", e);
+                    }}
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="flex flex-wrap">
+                  <p className="mr-5 text-lg font-medium text-white">Почта:</p>
+                  <InputText
+                    className="p-inputtext-sm grow"
+                    id="email"
+                    value={values.email}
+                    onChange={(e) => {
+                      handleChange("email", e);
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="mt-8">
+                <h4 className="mb-2 text-2xl font-bold text-white">
+                  Контактные данные
+                </h4>
+                <div className="mb-6 grid grid-cols-2 gap-4">
+                  <div className="flex flex-col gap-3">
+                    <label
+                      className="text-lg font-medium text-white"
+                      htmlFor="telegram"
+                    >
+                      Telegram
+                    </label>
+                    <InputText
+                      className="p-inputtext-sm"
+                      id="telegram"
+                      value={values.telegram}
+                      onChange={(e) => {
+                        handleChange("telegram", e);
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label
+                      className="text-lg font-medium text-white"
+                      htmlFor="whatsapp"
+                    >
+                      Whatsapp
+                    </label>
+                    <InputText
+                      className="p-inputtext-sm"
+                      id="whatsapp"
+                      value={values.whatsapp}
+                      onChange={(e) => {
+                        handleChange("whatsapp", e);
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label
+                      className="text-lg font-medium text-white"
+                      htmlFor="instagram"
+                    >
+                      Instagram
+                    </label>
+                    <InputText
+                      className="p-inputtext-sm"
+                      id="telegram"
+                      value={values.instagram}
+                      onChange={(e) => {
+                        handleChange("instagram", e);
+                      }}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <label
+                      className="text-lg font-medium text-white"
+                      htmlFor="phone"
+                    >
+                      Телефон
+                    </label>
+                    <InputText
+                      className="p-inputtext-sm"
+                      id="phone"
+                      value={values.phone}
+                      onChange={(e) => {
+                        handleChange("phone", e);
+                      }}
+                    />
+                  </div>
+                </div>
+                <CsButton filled onClick={updateUserData}>
+                  Сохранить изменения
+                </CsButton>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel header="Безопасность">
+            <div className="max-w-xl">
+              <h4 className="mb-2 text-2xl font-bold text-white">
+                Изменить пароль
+              </h4>
+              <div className="mb-6 grid grid-cols-2 gap-4">
+                <div className="flex flex-col gap-3">
+                  <label
+                    className="text-lg font-medium text-white"
+                    htmlFor="password"
+                  >
+                    Новый пароль
+                  </label>
+                  <Password
+                    className="p-inputtext-sm"
+                    id="password"
+                    value={securityValues.password}
+                    onChange={(e) => {
+                      handleSecurityChange("password", e);
+                    }}
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <label
+                    className="text-lg font-medium text-white"
+                    htmlFor="telegram"
+                  >
+                    Подтверждение пароля
+                  </label>
+                  <Password
+                    className="p-inputtext-sm"
+                    id="password_confirmation"
+                    value={securityValues.password_confirmation}
+                    onChange={(e) => {
+                      handleSecurityChange("password_confirmation", e);
+                    }}
+                  />
+                </div>
+              </div>
+              <CsButton filled onClick={updatePassword}>
+                Изменить пароль
+              </CsButton>
+            </div>
+          </TabPanel>
+        </TabView>
+      </div>
+    </AccountLayout>
+  );
+}
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -64,7 +64,7 @@ export const userRouter = createTRPCRouter({
 
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
-        message: "Проищошла ошибка при добавлении пользователя",
+        message: "Произошла ошибка при добавлении пользователя",
       });
     }),
   toggleFavorite: protectedProcedure
@@ -102,5 +102,98 @@ export const userRouter = createTRPCRouter({
         }
       }
       return { status: "error" };
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        first_name: z.string().min(1),
+        last_name: z.string().min(1),
+        phone: z.string(),
+        telegram: z.string(),
+        whatsapp: z.string(),
+        instagram: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const email = ctx.session.user.email;
+      if (email == null) {
+        throw new TRPCError({
+          message: "Не задан адрес e-mail",
+          code: "PARSE_ERROR",
+        });
+      }
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) {
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            email: input.email,
+            first_name: input.first_name,
+            last_name: input.last_name,
+            phone: input.phone,
+            social_networks: JSON.stringify({
+              instagram: input.instagram,
+              telegram: input.telegram,
+              whatsapp: input.whatsapp,
+            }),
+          },
+        });
+      }
+      return await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+    }),
+  updatePassword: protectedProcedure
+    .input(
+      z.object({
+        password: z.string().min(8),
+        password_confirmation: z.string().min(8),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const email = ctx.session.user.email;
+      if (email == null) {
+        throw new TRPCError({
+          message: "Не задан адрес e-mail",
+          code: "PARSE_ERROR",
+        });
+      }
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) {
+        if (input.password !== input.password_confirmation) {
+          return {
+            status: "error",
+            message: "Пароли не совпадают",
+          };
+        }
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            password: hashSync(input.password),
+          },
+        });
+        return {
+          status: "success",
+        };
+      }
+      return {
+        status: "error",
+        message: "Пользователь не найден",
+      };
     }),
 });
