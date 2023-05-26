0f59cf7750f6457ad3ddbebbe4025e4e8b7c849e
The Ghost <tghostlycat@gmail.com>
2023-05-26 06:48:01 +0400

    fix(types): better completion for UserWithRole type

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,7 +2,10 @@ import { type User, type UserReview, type UserRole } from "@prisma/client";
 
 export type Role = "admin" | "leaseholder" | "owner";
 export type UserWithRole = User & {
-  user_role: UserRole;
+  user_role: {
+    id: string;
+    name: Role;
+  };
 };
 export type UserReviewWithUserData = UserReview & {
   user: {
