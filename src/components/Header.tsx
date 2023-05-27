35bedfa6d87b0750b05a1d0b9259eb4a29da54c0
The Ghost <tghostlycat@gmail.com>
2023-05-27 19:06:28 +0400

    chore: add mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Hamburger from "hamburger-react";
 import Image from "next/image";
 import Link from "next/link";
 import { signOut, useSession } from "next-auth/react";
+import { Sidebar } from "primereact/sidebar";
 import React, { useEffect, useState } from "react";
 
 import CsButton from "@/components/CsButton";
@@ -78,7 +79,55 @@ export default function Header() {
             </>
           )}
         </div>
-        <div className="ml-auto block lg:hidden">
+        <div className="ml-auto flex gap-x-4 lg:hidden">
+          <Sidebar
+            visible={isMenuOpen}
+            position="right"
+            onHide={() => {
+              setMenuOpen(false);
+            }}
+          >
+            <div className="flex h-full flex-col">
+              <ul className="flex flex-col flex-wrap gap-y-6">
+                <HeaderFooterMenuLink href="/" text="Главная" />
+                <HeaderFooterMenuLink href="/platforms" text="Площадки" />
+              </ul>
+              <div className="mt-auto flex justify-around">
+                {status !== "authenticated" && (
+                  <>
+                    <CsButton type="link" href="/user/register">
+                      Регистрация
+                    </CsButton>
+                    <CsButton type="link" href="/user/login">
+                      Войти
+                    </CsButton>
+                  </>
+                )}
+                {status === "authenticated" && userFullName !== "" && (
+                  <>
+                    <CsButton type="link" href="/user/">
+                      {userFullName}
+                    </CsButton>
+                    <CsButton
+                      onClick={() => {
+                        void signOut({ callbackUrl: "/" });
+                      }}
+                    >
+                      Выйти
+                    </CsButton>
+                  </>
+                )}
+              </div>
+            </div>
+          </Sidebar>
+          <Link className="block" href="/user/favorites">
+            <Image src={LikeDark} alt="" height={40} width={40} />
+          </Link>
+          <Link className="block" href="messages">
+            <div className="flex h-10 w-10 items-center justify-center">
+              <Image src={HeaderChatIcon} alt="" height={22} width={22} />
+            </div>
+          </Link>
           <Hamburger toggled={isMenuOpen} toggle={setMenuOpen} color="#fff" />
         </div>
       </div>
