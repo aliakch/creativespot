d0d1105860923af48f088552b0ee29776edc7603
The Ghost <tghostlycat@gmail.com>
2023-06-09 23:54:46 +0400

    fix: user settings display

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -10,8 +10,8 @@ export default function UserDashboardPage() {
     <>
       {me !== false && (
         <AccountLayout selectedOption="general" role={me.user_role.name}>
-          <div className="grid grid-cols-4 gap-x-8">
-            <div className="col-span-3 flex items-center rounded-2xl bg-cs-dark-600 p-5">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-4 lg:grid-cols-4">
+            <div className="flex items-center rounded-2xl bg-cs-dark-600 p-5 lg:col-span-3">
               <CsAvatar className="mr-8" size="big" label={me.first_name} />
               <div>
                 <h5 className="mb-2 text-2xl font-medium text-white">
